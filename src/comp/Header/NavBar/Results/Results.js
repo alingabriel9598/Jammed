ee97f5bec4a7605afec31db5baa95ac2d33a5685
Alin Gabriel <alingabriel9598@gmail.com>
2023-10-25 00:30:46 +0300

    Search tracks on Spotify

--- src/comp/Header/NavBar/Results/Results.js
+++ src/comp/Header/NavBar/Results/Results.js
@@ -1,40 +1,36 @@
-import React,{useState} from "react";
+import React,{ useState, useEffect } from "react";
 import styles from './Results.module.css'
-import songs from '../../../../utils/Local'
-import { BsPlus } from "react-icons/bs";
+import Spotify from "../../../../utils/Spotify";
 
-function Results({ searchInput, onSelectSong }){
-    // Filter the songs based on the searchInput
-    const filteredSongs = songs.filter((song) => {
-    // Perform a case-insensitive search by converting both the search input and song name to lowercase
-    const searchValue = searchInput.toLowerCase();
-    const songName = song.name.toLowerCase();
-    const songArtist = song.artist.toLowerCase();
-    const songAlbum = song.album.toLowerCase();
-    // Return songs that contain the search input as a substring in their name
-    return (
-      songName.includes(searchValue) ||
-      songArtist.includes(searchValue) ||
-      songAlbum.includes(searchValue)
-    );
-  });
-    const [confirmationMessage, setConfirmationMessage] = useState(''); // State for the confirmation message
+function Results({ searchInput, onSongAdded }){
+    const [searchResults, setSearchResults] = useState([]);
+    useEffect(() => {
+      // Clear search results before updating
+      setSearchResults([]);
+  
+      // Execute search when searchInput changes
+      if (searchInput) {
+        Spotify.search(searchInput).then((results) => {
+          setSearchResults(results);
+        });
+      }
+    }, [searchInput]);
     return (
         <div>
           <section className={styles.section}>
             <h1>Results for {searchInput}</h1>
-            {filteredSongs.length > 0 ? (
-               <div>
-                   {filteredSongs.map((song) => (
-                   <p key={song.id}>
-                     {song.name} - {song.artist} - {song.album}
-                    <div><button onClick={() => {onSelectSong(song);setConfirmationMessage(`The song has been added to the playlist`);setTimeout(() => setConfirmationMessage(''), 3000);}} className={styles.button}><BsPlus/>Add to Playlist</button></div>
-                   </p>))}
-               </div>
+            {searchResults.length > 0 ? (
+          <ul>
+            {searchResults.map((song) => (
+              <li key={song.id}>
+                {song.name} - {song.artist} - {song.album}
+                <div><button className={styles.button}onClick={() => onSongAdded(song)}>Add to Playlist</button></div>
+              </li>
+            ))}
+          </ul>
         ) : (
           <p>No results found.</p>
         )}
-          {confirmationMessage && <p>{confirmationMessage}</p>}
           </section>
         </div>
     )
